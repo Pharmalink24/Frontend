120f2c8646805755392142286cb004751dbd2f21
MoErn878 <mohamed_mustafa@stud.cu.edu.eg>
2024-04-21 20:25:18 +0200

    Refactor CardContainerWithTitle widget in card_container_column.dart

--- lib/core/widgets/card_container_column.dart
+++ lib/core/widgets/card_container_column.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:pharmalink/core/theme/colors.dart';
 import 'package:pharmalink/core/theme/styles.dart';
@@ -10,41 +9,40 @@ class CardContainerWithTitle extends StatelessWidget {
   final String title;
   final TextStyle? style;
   final Widget child;
+  final Widget iconButton;
+
   const CardContainerWithTitle({
     super.key,
     required this.title,
     this.style,
     required this.child,
+    this.iconButton = const SizedBox.shrink(),
   });
 
   @override
   Widget build(BuildContext context) {
     return CardContainer(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              title,
-              style: style ??
-                  AppTextStyle.bodyMedium.copyWith(
-                    fontFamily: AppFonts.secondary,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w300,
-                  ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu),
-              color: AppColors.secondaryText,
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 12.0),
-          child: child,
-        ),
-      ],
+      title: title,
+      iconButton: iconButton,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: style ??
+                AppTextStyle.bodyMedium.copyWith(
+                  fontFamily: AppFonts.secondary,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w300,
+                ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+            color: AppColors.secondaryText,
+          ),
+        ],
+      ),
     );
   }
 }
