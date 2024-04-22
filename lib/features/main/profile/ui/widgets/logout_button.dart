79f0f6e3d77bc9c54f000f1573428af7ab89c831
MoErn878 <mohamed_mustafa@stud.cu.edu.eg>
2024-04-22 13:53:08 +0200

    Done Logout

--- lib/features/main/profile/ui/widgets/logout_button.dart
+++ lib/features/main/profile/ui/widgets/logout_button.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pharmalink/core/di/dependency_injection.dart';
 import 'package:pharmalink/core/routes/routes.dart';
+import 'package:pharmalink/core/shared_preferences/auth_prefs.dart';
 import 'package:pharmalink/core/theme/colors.dart';
 import 'package:pharmalink/core/theme/styles.dart';
 import 'package:pharmalink/features/access/auth/logic/cubit/auth_cubit.dart';
-import 'package:pharmalink/features/access/signin/logic/cubit/signin_cubit.dart';
 
 class LogoutButton extends StatelessWidget {
   const LogoutButton({
@@ -24,25 +25,68 @@ class LogoutButton extends StatelessWidget {
             showDialog(
               context: context,
               builder: (context) => AlertDialog(
-                title: const Text('Log Out'),
-                content: const Text('Are you sure you want to log out?'),
+                title: const Text(
+                  'Log Out',
+                  style: AppTextStyle.headlineSmall,
+                ),
+                content: Text(
+                  'Are you sure you want to log out?',
+                  style: AppTextStyle.bodyLarge.copyWith(fontSize: 20.0),
+                ),
                 actions: [
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    child: const Text('Cancel'),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      // Close dialog
-                      Navigator.of(context).pop();
-                      // Log out
-                      context.read<AuthCubit>().logout();
-                      // Navigate to sign in screen
-                      Navigator.of(context)
-                          .pushReplacementNamed(Routes.signInScreen);
-                    },
-                    child: const Text('Log Out'),
-                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        style: ButtonStyle(
+                          backgroundColor: const MaterialStatePropertyAll(
+                            AppColors.primary,
+                          ),
+                          shape: MaterialStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          elevation: const MaterialStatePropertyAll(5.0),
+                        ),
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: Text(
+                          'Cancel',
+                          style: AppTextStyle.titleSmall.copyWith(
+                            color: AppColors.primaryText,
+                          ),
+                        ),
+                      ),
+                      // Todo: Implement log out with AuthCubit
+                      TextButton(
+                        style: ButtonStyle(
+                          backgroundColor: const MaterialStatePropertyAll(
+                            AppColors.secondary,
+                          ),
+                          shape: MaterialStatePropertyAll(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          elevation: const MaterialStatePropertyAll(5.0),
+                        ),
+                        onPressed: () async {
+                          // Close dialog
+                          Navigator.of(context).pop();
+                          // Log out
+                          AuthSharedPrefs
+                              .clearAuthData(); // Navigate to sign in screen
+                          Navigator.of(context)
+                              .pushReplacementNamed(Routes.signInScreen);
+                        },
+                        child: const Text(
+                          'Log Out',
+                          style: AppTextStyle.titleSmall,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
             );
