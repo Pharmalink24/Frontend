4c6d661a0a0a09f32d7cdb6b82c4ce667e7b370b
Mohamed Bahaa <mohamed.abdelsalam00@eng-st.cu.edu.eg>
2024-04-27 01:04:30 +0300

    doctor info

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,13 @@
-// Flutter Packages
 import 'package:flutter/material.dart';
-// Screens
-import 'package:pharmalink/screens/welcome_screen.dart';
-import 'package:pharmalink/utilities/constants.dart';
-// Packages
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'pharmalink_app.dart';
+import 'package:pharmalink/core/routes/app_router.dart';
+import 'init.dart';
 
-void main() => runApp(const MainApp());
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "PharmaLink",
-      theme: ThemeData().copyWith(
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-            fontFamily: AppFonts.primary,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-      ),
-      initialRoute: WelcomeScreen.url,
-      routes: {
-        WelcomeScreen.url: (context) => const WelcomeScreen(),
-      },
-    );
-  }
+void main() async {
+  await init();
+  runApp(
+    PharmalinkApp(
+      appRouter: AppRouter(),
+    ),
+  );
 }
